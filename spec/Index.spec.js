6f344c1255ab67482aeeeb5a992c97da5b837d14
ivalentine <ian@ivalentine.net>
2016-03-17 11:30:30 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -12,8 +12,8 @@ describe('angularjs homepage todo list', function() {
 		var names = element.all(by.model('contact.name'));
 		var phones = element.all(by.model('contact.phone'));
 		expect(contacts.count()).toEqual(4);
-		expect(names.get(1)).toEqual('Tim');
-		expect(phones.get(1)).toEqual('3934203242');
+		expect(names.get(1).getAttribute('value')).toEqual('Tim');
+		expect(phones.get(1).getAttribute('value')).toEqual('3934203242');
 
 	});
 });
